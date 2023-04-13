77db39f4c06c05059c9f153f5b3451582ab70e02
Jymapas <Jymapas@yandex.ru>
2023-04-13 17:37:32 +0300

    Пробная версия, которая не работает, но хотя бы собирается и компилируется. Создал RelayCommand, попытался на него завязать кнопки.

--- TimerKVRM/Models/TimerModel.cs
+++ TimerKVRM/Models/TimerModel.cs
@@ -1,6 +1,27 @@
-﻿namespace TimerKVRM.Models
+﻿using System;
+using System.Windows.Threading;
+
+namespace TimerKVRM.Models
 {
     internal class TimerModel
     {
+        private DispatcherTimer _timer;
+        private int _displayTime;
+
+        public TimerModel()
+        {
+            _timer = new DispatcherTimer();
+        }
+
+        public void Start(int seconds)
+        {
+            _displayTime = seconds;
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
     }
 }
--- TimerKVRM/ViewModels/TimerViewModel.cs
+++ TimerKVRM/ViewModels/TimerViewModel.cs
@@ -1,8 +1,57 @@
-﻿using TimerKVRM.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Windows.Threading;
+using TimerKVRM.Models;
 
 namespace TimerKVRM.ViewModels
 {
     internal class TimerViewModel
     {
+        private DispatcherTimer _timer;
+
+        public int DisplayTime { get; set; }
+
+        public ICommand QuestionCommand { get; }
+        public ICommand StopCommand { get; }
+
+        public TimerViewModel()
+        {
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+
+            _timer.Tick += OnTimeChanged;
+
+            DisplayTime = 60;
+
+            QuestionCommand = new RelayCommand(Question);
+            StopCommand = new RelayCommand(StopTimer);
+        }
+
+        private void Question()
+        {
+            DisplayTime = 60;
+            _timer.Start();
+        }
+
+        private void StopTimer()
+        {
+            _timer.Stop();
+            DisplayTime = 60;
+        }
+
+        private void OnTimeChanged(object sender, EventArgs e)
+        {
+            DisplayTime--;
+
+            if (DisplayTime < 1)
+            {
+                StopTimer();
+            }
+        }
     }
 }
